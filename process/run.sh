1f8a69a6b36db67dddf5a5c38af42be7be58deff
Garrett Wilson <g@floft.net>
2019-10-22 11:17:57 -0700

    script to run all

--- process/run.sh
+++ process/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#/usr/bin/time ./process.py --dir=test_data/ --nums=1
 /usr/bin/time ./process.py --dir="/home/garrett/Documents/School/19_Summer_Large/Mobile Activity Study/al-watch-log-upload-api-new" --nums=1,2,3,4,5,6,7,7,8,9,10,12,13,14,15
